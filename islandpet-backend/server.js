eb49a92708cfd8df14f1d424e51ea0bfae6065c4
Bailey Kiehl <bailey.d.kiehl@gmail.com>
2025-07-11 16:42:32 -0500

    uncaught error handler

--- islandpet-backend/server.js
+++ islandpet-backend/server.js
@@ -1,4 +1,14 @@
+// Add this block to the top of server.js
+process.on('uncaughtException', (err, origin) => {
+    console.error('!!!!!!!!!! UNCAUGHT EXCEPTION !!!!!!!!!!!');
+    console.error('Error:', err);
+    console.error('Origin:', origin);
+    // It's recommended to exit gracefully after an uncaught exception
+    process.exit(1);
+});
+
 import app from './app.js';
+
 const PORT = process.env.PORT || 8080;
 console.log('[server] NODE_ENV:', process.env.NODE_ENV);
 console.log('[server] APNS_ENV =', process.env.APNS_ENV);
